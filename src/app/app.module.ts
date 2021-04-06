e6a7a39f14a5bff7dfd8787babee6e0dacfb6c13
Andrew Siachos <andrewsiaxos@yahoo.gr>
2021-04-06 12:35:35 +0300

    mobile design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ActiveComponent } from './active/active.component';
-import { InactiveComponent } from './inactive/inactive.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ActiveComponent,
-    InactiveComponent
   ],
   imports: [
     BrowserModule,
